ab85b632c8ef554dd49a85e7db067bcc2a7eca2b
Nathan Genetzky <nathan@genetzky.us>
2017-11-12 21:57:52 -0600

    dev-base: Simplify and remove user 'dev'.

--- dev-base/dev-base.sh
+++ dev-base/dev-base.sh
@@ -1,16 +1,19 @@
 
+GIT_ROOT="$(git rev-parse --show-toplevel)"
+
 build()
 {
     sudo docker build \
-        -t dev-base:0.1 ./dev-base
+        -t dev-base:0.0.2 \
+        "${GIT_ROOT}/dev-base/dev-base"
 }
 
 run()
 {
     sudo docker run \
         -it \
-        -v $(pwd):$(pwd) \
-        dev-base:0.1
+        -v $(pwd):/mnt/pwd \
+        dev-base:0.0.2
 
 }
 
